4b6d4572a12cad1e17f77fe7a84e76167c4f7788
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-11-02 21:28:09 +0900

    feat(InputValidator): 보너스 번호 검증 기능 추가
    - 숫자 형식 검증
    - 1-45 범위 검증
    - 당첨 번호와 중복 검증

--- src/validators/InputValidator.js
+++ src/validators/InputValidator.js
@@ -60,6 +60,35 @@ class InputValidator {
       throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBERS);
     }
   }
+
+  static validateBonusNumber(input, winningNumbers) {
+    const bonusNumber = this.#parseBonusNumber(input);
+    this.#validateBonusNumberRange(bonusNumber);
+    this.#validateBonusNumberDuplication(bonusNumber, winningNumbers);
+    return bonusNumber;
+  }
+
+  static #parseBonusNumber(input) {
+    const bonusNumber = Number(input.trim());
+
+    if (Number.isNaN(bonusNumber)) {
+      throw new Error(ERROR_MESSAGES.INVALID_BONUS_NUMBER_TYPE);
+    }
+
+    return bonusNumber;
+  }
+
+  static #validateBonusNumberRange(bonusNumber) {
+    if (bonusNumber < LOTTO.MIN_NUMBER || bonusNumber > LOTTO.MAX_NUMBER) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBER_RANGE);
+    }
+  }
+
+  static #validateBonusNumberDuplication(bonusNumber, winningNumbers) {
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGES.INVALID_BONUS_NUMBER_DUPLICATE);
+    }
+  }
 }
 
 export default InputValidator;
