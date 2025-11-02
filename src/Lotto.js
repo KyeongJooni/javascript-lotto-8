c06f9c0bac441ac618b5414517431c8cf5c7cc0d
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-11-02 20:59:53 +0900

    feat(Lotto): 로또 번호 검증 로직 추가
    - 로또 번호 개수 검증
    - 로또 번호 범위 검증
    - 로또 번호 중복 검증
    - 상수 파일에서 값 import하여 하드코딩 제거
    - getNumbers() 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { LOTTO, ERROR_MESSAGES } from './constants/lottoConstants.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,36 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    this.#validateCount(numbers);
+    this.#validateRange(numbers);
+    this.#validateDuplication(numbers);
+  }
+
+  #validateCount(numbers) {
+    if (numbers.length !== LOTTO.NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBERS_COUNT);
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateRange(numbers) {
+    const hasInvalidRange = numbers.some(
+      (number) => number < LOTTO.MIN_NUMBER || number > LOTTO.MAX_NUMBER
+    );
+    if (hasInvalidRange) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBER_RANGE);
+    }
+  }
+
+  #validateDuplication(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBERS);
+    }
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
